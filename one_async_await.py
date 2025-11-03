0619726898b9cbde158557ad5f977d401a9057c3
Aleksey242424 <alekseysokolovsky_256@mail.ru>
2025-11-03 11:04:06 -0900

    feature/async,await

---
+++ one_async_await.py
@@ -0,0 +1,30 @@
+import asyncio
+
+def get_docs():
+    docs = '''
+async это маркер для питона что функция возвращает не обычные значения, а корутину
+    
+await нужен для передачи управления обратно в event loop, когда корутина встречает операцию требующую ожидания.
+Технический это выглядит так,"Я сейчас буду ждать результат этой операции - пока я жду можешь выполнять другие операции"
+    '''
+    return docs
+
+
+#синхронная функция, вернёт 777
+def sync_func():
+    return 777
+
+
+#асинхронная функция, вернёт объект корутины (<coroutine object async_func at 0x7f823f8b2610>)
+async def async_func():
+    return
+
+async def main():
+    print(get_docs())
+    print(sync_func())
+    print(async_func())
+
+    
+asyncio.run(main())
+
+
